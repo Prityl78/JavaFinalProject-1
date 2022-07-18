50567a598f83ad92fa71ec14b7ff2a213dbdca61
Eyerusalem Daniel <edegefu94@gmail.com>
2022-07-18 10:01:15 -0400

    initial commit

---
+++ test/com/generation/service/CourseServiceTest.java
@@ -0,0 +1,29 @@
+package com.generation.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void registerCourse() {
+    }
+
+    @Test
+    void getCourse() {
+    }
+
+    @Test
+    void enrollStudent() {
+    }
+
+    @Test
+    void showEnrolledStudents() {
+    }
+}
---
+++ test/com/generation/service/StudentServiceTest.java
@@ -0,0 +1,35 @@
+package com.generation.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void subscribeStudent() throws ParseException {
+
+
+
+
+
+    }
+
+    @Test
+    void findStudent() {
+    }
+
+    @Test
+    void enrollToCourse() {
+    }
+}
